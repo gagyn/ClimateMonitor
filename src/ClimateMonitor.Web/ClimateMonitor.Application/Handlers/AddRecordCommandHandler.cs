e23481a53bf1edf9efd1f68bde4de6a752106d5b
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2024-03-27 22:43:20 +0100

    add using user context

--- src/ClimateMonitor.Web/ClimateMonitor.Application/Handlers/AddRecordCommandHandler.cs
+++ src/ClimateMonitor.Web/ClimateMonitor.Application/Handlers/AddRecordCommandHandler.cs
@@ -1,3 +1,4 @@
+using ClimateMonitor.Application.Authorization;
 using ClimateMonitor.Application.Commands;
 using ClimateMonitor.Domain.Entities;
 using ClimateMonitor.Domain.Repositories;
@@ -5,14 +6,18 @@ using MediatR;
 
 namespace ClimateMonitor.Application.Handlers;
 
-public class AddRecordCommandHandler(IRecordRepository recordRepository, TimeProvider timeProvider) : IRequestHandler<AddRecordCommand>
+public class AddRecordCommandHandler(
+    IRecordRepository recordRepository,
+    TimeProvider timeProvider,
+    IUserContext userContext) : IRequestHandler<AddRecordCommand>
 {
     private readonly IRecordRepository recordRepository = recordRepository;
     private readonly TimeProvider timeProvider = timeProvider;
+    private readonly IUserContext userContext = userContext;
     
     public async Task Handle(AddRecordCommand request, CancellationToken cancellationToken)
     {
-        var deviceId = Guid.Empty; // todo: implement user context
+        var deviceId = userContext.Id;
         var record = RecordEntity.Create(deviceId, request.SensorId, request.ReadAt, request.Temperature,
             request.Humidity, timeProvider, "createdBy");// todo
         
