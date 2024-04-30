07000919037b0521ba5b7093dccea917db0a3f99
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2024-04-30 23:50:53 +0200

    add settings page template

--- src/ClimateMonitor.Web/ClimateMonitor.Web/ClientApp/src/App.tsx
+++ src/ClimateMonitor.Web/ClimateMonitor.Web/ClientApp/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Settings } from './pages/settings';
 
 function App() {
   return (
     <div className="App">
+      <Settings />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
