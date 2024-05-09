e18fd1c117d6a412debe47e10b560a6514cf1f1e
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2024-05-09 23:48:42 +0200

    add devices

--- src/ClimateMonitor.Web/ClimateMonitor.Web/ClientApp/src/components/settings/devices.tsx
+++ src/ClimateMonitor.Web/ClimateMonitor.Web/ClientApp/src/components/settings/devices.tsx
@@ -1,8 +1,16 @@
 import Grid from "@mui/material/Grid";
+import { DeviceModel } from "../../models/deviceModel";
+import { Button } from "@mui/material";
 
-export function Devices() {
+type DevicesProps = {
+    devices: DeviceModel[]
+}
+
+export function Devices({ devices }: DevicesProps) {
     return (
         <Grid>
-
+            {devices?.map((x) => (
+                <p><Button variant="contained">Edit device {x.deviceId}</Button></p>
+            ))}
         </Grid>)
 }
--- src/ClimateMonitor.Web/ClimateMonitor.Web/ClientApp/src/pages/settings.tsx
+++ src/ClimateMonitor.Web/ClimateMonitor.Web/ClientApp/src/pages/settings.tsx
@@ -1,11 +1,10 @@
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
-import { LocationBody } from "../components/location-body";
-import { LocationHeader } from "../components/location-header";
 import { NavigationPanel } from "../components/navigation-panel";
 import { useDevices } from "../hooks/useDevices";
 import { useMyUser } from "../hooks/useMyUser";
+import { Devices } from "../components/settings/devices";
 
 export function Settings() {
   const devices = useDevices();
@@ -19,9 +18,7 @@ export function Settings() {
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Container>
           <p>Settings</p>
-          {devices?.map((x) => (
-            <p>{x.deviceId}</p>
-          ))}
+          <Devices devices={devices ?? []} />
         </Container>
       </Box>
     </>
